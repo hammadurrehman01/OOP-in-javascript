f953d597ac53f0daf2bd3e7cc03b40e44df662ea
hammadurrehman01 <hammadurrehman1954@gmail.com>
2024-09-20 20:51:33 +0500

    changex

--- app.js
+++ app.js
@@ -2,43 +2,63 @@ const fullnameInput = document.getElementById('fullname');
 const initialBalanceInput = document.getElementById('initialBalance');
 const createAccountForm = document.getElementById('createAccountForm');
 
-// Get elements from Deposit Form
+
 const depositAccountNumberInput = document.getElementById('depositAccountNumber');
 const depositAmountInput = document.getElementById('depositAmount');
 const depositForm = document.getElementById('depositForm');
 
-// Get elements from Credit Form
+
 const creditAccountNumberInput = document.getElementById('creditAccountNumber');
 const creditAmountInput = document.getElementById('creditAmount');
 const creditForm = document.getElementById('creditForm');
 
-// Example: Add event listeners to the forms
+
+const accounts = []
+
+
+
+function CreateBankAccount(customerName, balance) {
+  this.customerName = customerName;
+  this.accountNumber = Date.now();
+  this.balance = balance ?? 0;
+
+  this.deposit = (amount) => {
+    const accountExists = accounts.find((account) => account.accountNumber === Number(depositAccountNumberInput.value))
+    if (!accountExists) {
+      console.log("This account is not exists");
+      return;
+    }
+    accountExists.balance = accountExists.balance + amount
+    console.log(accounts);
+
+  }
+}
+
+
+
 createAccountForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log('Full Name:', fullnameInput.value);
-  console.log('Initial Balance:', initialBalanceInput.value);
+
+  const userAccount = new CreateBankAccount(fullnameInput.value, Number(initialBalanceInput.value));
+  accounts.push(userAccount)
+  console.log(accounts);
+  fullnameInput.value = ''
+  initialBalanceInput.value = ''
+
 });
 
 depositForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log('Deposit Account Number:', depositAccountNumberInput.value);
-  console.log('Deposit Amount:', depositAmountInput.value);
+
+  const userAccount = new CreateBankAccount();
+  userAccount.deposit(Number(depositAmountInput.value))
 });
 
 creditForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  console.log('Credit Account Number:', creditAccountNumberInput.value);
-  console.log('Credit Amount:', creditAmountInput.value);
 });
 
-function CreateBankAccount(customerName, balance) {
-    this.customerName = customerName;
-    this.accountNumber = Date.now();
-    this.balance = balance ?? 0;
-}
 
 
-const hammadAccount = new CreateBankAccount("Hammad Ur Rehman", 1000);
-const rakeshAccount = new CreateBankAccount("Rakesh k", 1000);
 
-console.log(hammadAccount, rakeshAccount);
+// console.log(hammadAccount, rakeshAccount);
