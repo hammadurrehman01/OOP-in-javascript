ed153def83942915683e154359d68c311ac67090
hammadurrehman01 <hammadurrehman1954@gmail.com>
2024-10-01 21:21:44 +0500

    static methods

---
+++ static.js
@@ -0,0 +1,26 @@
+class User {
+
+    constructor(name, age, income = 0) {
+        this.name = name;
+        this.age = age;
+        this.income = income;
+    }
+
+    static compareByAge(a, b) {
+        return a.age - b.age
+    }
+
+    static compareByIncome(a, b) {
+        return a.income - b.income
+    }
+}
+
+const user1 = new User('Ali Ahmed', 30, 10000);
+const user2 = new User('Hammad Ur Rehman', 20, 14000);
+const user3 = new User('Mubeen Khan', 40, 30000);
+
+const users = [user1, user2, user3];
+// users.sort(User.compareByAge)
+users.sort(User.compareByIncome)
+
+console.log(users); 
