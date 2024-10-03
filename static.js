8dfe9ce7addc56bada7007f6186d469bb3695646
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-10-03 10:00:16 +0500

    static properties and methods

--- static.js
+++ static.js
@@ -13,14 +13,30 @@ class User {
     static compareByIncome(a, b) {
         return a.income - b.income
     }
+
+    static customerName() {
+        console.log("Hammad Ur Rehman");
+    }
+
+    static customerAge() {
+        this.customerName()
+        console.log(20);
+    }
+
+    static {
+        console.log("Initialized!");
+
+    }
 }
 
-const user1 = new User('Ali Ahmed', 30, 10000);
-const user2 = new User('Hammad Ur Rehman', 20, 14000);
-const user3 = new User('Mubeen Khan', 40, 30000);
+// const user1 = new User('Ali Ahmed', 30, 10000);
+// const user2 = new User('Hammad Ur Rehman', 20, 14000);
+// const user3 = new User('Mubeen Khan', 40, 30000);
 
-const users = [user1, user2, user3];
+// const users = [user1, user2, user3];
 // users.sort(User.compareByAge)
-users.sort(User.compareByIncome)
+// users.sort(User.compareByIncome)
 
-console.log(users); 
+//console.log(users); 
+
+User.customerAge();
